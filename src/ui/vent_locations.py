7de5c97a2934ee9a5635758e57b8e49e81df2495
Melvin Kaneshiro <melvin.kaneshiro@yahoo.com>
2025-11-25 03:20:06 -1000

    Fixed nested repo and migrating to new remote

--- src/ui/vent_locations.py
+++
@@ -1,8 +0,0 @@
-#vent_locations.py 
-
-vent_locations = [
-    {"name": "Moku‘āweoweo Summit Vent", "coords": [19.475, -155.608]},
-    {"name": "Southwest Rift Zone Vent", "coords": [19.383, -155.625]},
-    {"name": "Northeast Rift Zone Vent", "coords": [19.525, -155.500]},
-    {"name": "Kilauea Summit Caldera", "coords": [19.406, -155.283]},
-]
