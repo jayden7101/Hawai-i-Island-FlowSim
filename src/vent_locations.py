ecbc34af029410a660d6f19df1a6947ea981c4e7
dacotah <dacotah.moline.livdahl@gmail.com>
2026-02-04 06:59:09 +0000

    Added hardcoded lat/lon array + vent json array. adjusted vent_utils to account for changes and tracking closest vent location index

--- src/vent_locations.py
+++ src/vent_locations.py
@@ -1,9 +1,44 @@
 #vent_locations.py 
 
-vent_locations = [
-    [19.1859, -155.7437], 
-    [19.23748, -155.74508],
-    [19.26197, -155.7398],
-    [19.1275, -155.7349], 
-    [19.5591, -155.2292],
+"""
+these are the coordinates of vents. the coordinates for the
+vents should be in the same array position as the json file
+file for the vents. a comment after each lat/lon will say which
+vent/json file it belongs to
+"""
+vent_lat_lon = [
+    [19.1859, -155.7437],   #mlv5
+    [19.0541, -155.6781],   #mlv9
+    [19.3213, -155.709],    #mlv21
+    [19.3408, -155.6947],   #mlv27
+    [19.5591, -155.2292],   #mlv59
+    [19.1681, -155.7365],   #mlv12
+    [19.5248, -155.7365],   #mlv60
+    [19.534, -155.2064],    #mlv1000
+    [19.1267, -155.7367],   #mlv68
+    [19.0862, -155.7292],   #mlv2
+    [19.5432, -155.2193],   #mlv1001
+    [19.0798, -155.6923],   #mlv10
+    [19.1784, -155.7295],   #mlv17
+    [19.522, -155.4338]     #mlv65
 ]
+
+vent_jsons = [
+    "mlv5.json",
+    "mlv9.json",
+    "mlv21.json",
+    "mlv27.json",
+    "mlv59.json",
+    "mlv12.json",
+    "mlv60.json",
+    "mlv1000.json",
+    "mlv68.json",
+    "mlv2.json",
+    "mlv1001.json",
+    "mlv10.json",
+    "mlv17.json",
+    "mlv65.json"
+    
+]
+
+vent_lat_lon = vent_jsons
--- src/vent_utils.py
+++ src/vent_utils.py
@@ -1,5 +1,5 @@
 from math import radians, sin, cos, sqrt, atan2
-from vent_locations import vent_locations
+from vent_locations import vent_lat_lon, vent_jsons
 
 def haversine_distance(coord1, coord2):
     """Return the distance between two lat/lon pairs in kilometers."""
@@ -15,13 +15,17 @@ def haversine_distance(coord1, coord2):
 
 def find_closest_vent(user_coord):
     """Return the closest vent (dict) to the given [lat, lon]."""
-    closest = None
+    closest_coord = None
+    closest_index = None 
     min_distance = float('inf')
 
-    for vent_coord in vent_locations:
+    for idx, vent_coord in enumerate(vent_lat_lon):
         dist = haversine_distance(user_coord, vent_coord)
         if dist < min_distance:
             min_distance = dist
-            closest = vent_coord
+            closest_coord = vent_coord
+            closest_index = idx
 
-    return closest, round(min_distance, 2)
+            vent_file = vent_jsons[closest_index] if closest_index is not None else None
+
+    return closest_coord, round(min_distance, 2), closest_index, vent_file
