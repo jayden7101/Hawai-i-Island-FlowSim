7382fc61f4f00185147d9c52968269586401de8a
Dacotah Moline Livdahl <dacotah.moline.livdahl@gmail.com>
2026-02-28 01:14:35 -1000

    added json+animations of vents that do not yet have all animations made; updated vent locations.

--- src/vent_locations.py
+++ src/vent_locations.py
@@ -1,29 +1,37 @@
 #vent_locations.py 
-
 """
 these are the coordinates of vents. the coordinates for the
 vents should be in the same array position as the json file
 file for the vents. a comment after each lat/lon will say which
 vent/json file it belongs to
+add all new vents/coords/json files to the end of the array
 """
+
 vent_lat_lon = [
-    #[19.1859, -155.7437],   #mlv5
-    [19.0541, -155.6781],   #mlv9
-    [19.3213, -155.709],    #mlv21
-    [19.3408, -155.6947],   #mlv27
-    #[19.5591, -155.2292],   #mlv59
-    [19.1681, -155.7365],   #mlv12
-    [19.5248, -155.7365],   #mlv60
-    [19.534, -155.2064],    #mlv1000
-    [19.1267, -155.7367],   #mlv68
-    [19.0862, -155.7292],   #mlv2
-    [19.5432, -155.2193],   #mlv1001
-    [19.0798, -155.6923],   #mlv10
-    [19.1784, -155.7295],   #mlv17
-    [19.522, -155.4338],    #mlv65
-    [19.2061, -155.7449],   #mlv5014
-    [19.1867, -155.7427],   #mlv5012
-    #[19.2255, -155.7453]    #mlv5011                    
+    #[19.1859, -155.7437],  #mlv5: missing dem
+    [19.0541, -155.6781],   #mlv9 .
+    [19.3213, -155.709],    #mlv21 .
+    [19.3408, -155.6947],   #mlv27 .
+    [19.5591, -155.2292],   #mlv59 .
+    [19.1681, -155.7365],   #mlv12 .
+    [19.5248, -155.7365],   #mlv60 .
+    [19.534, -155.2064],    #mlv1000 .
+    [19.1267, -155.7367],   #mlv68 .
+    [19.0862, -155.7292],   #mlv2 .
+    [19.5432, -155.2193],   #mlv1001 .
+    [19.0798, -155.6923],   #mlv10 .
+    [19.1784, -155.7295],   #mlv17 .
+    [19.522, -155.4338],    #mlv65 .
+    [19.2061, -155.7449],   #mlv5014 .
+    [19.1867, -155.7427],   #mlv5012 .
+    [19.2255, -155.7453],   #mlv5011 .
+    [19.2603, -155.7431],   #mlv5013 .
+    [19.3551, -155.6779],   #mlv5018 .
+    [19.3110, -155.7167],   #mlv5015 .
+    [19.23748, -155.74508], #mlv14 .
+    [19.5202, -155.2110],   #mlv1002 .
+    [19.5156, -155.2358]    #mlv1003 .
+    
 ]
 
 vent_jsons = [
@@ -31,7 +39,7 @@ vent_jsons = [
     "mlv9.json",
     "mlv21.json",
     "mlv27.json",
-    #"mlv59.json",
+    "mlv59.json",
     "mlv12.json",
     "mlv60.json",
     "mlv1000.json",
@@ -43,7 +51,11 @@ vent_jsons = [
     "mlv65.json",
     "mlv5014.json",
     "mlv5012.json",
-    #"mlv5011.json" 
+    "mlv5011.json",
+    "mlv5013.json",
+    "mlv5018.json",
+    "mlv5015.json",
+    "mlv14.json",
+    "mlv1002.json",
+    "mlv1003.json"
 ]
-
-# vent_lat_lon = vent_jsons
